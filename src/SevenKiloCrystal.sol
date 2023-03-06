45a628d79d8d0cb59c9fe059a6a71e2562ca2b1f
exp_table <exp.table@protonmail.com>
2023-03-06 11:10:07 +0100

    Can you cook?

---
+++ src/SevenKiloCrystal.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+library SevenKiloCrystal {
+
+    function cook() internal pure returns (bytes memory kilo) {
+        assembly {
+            mstore(kilo, 0x40)
+            mstore(add(kilo, 0x20), 0)
+            mstore(0x40, add(mload(0x40), 0x20))
+        }
+    }
+
+    function addition(bytes memory crystal, uint256 compound) internal returns (bytes memory) {
+        assembly {
+            // from vectorized/solady
+            function clz(x) -> r {
+                let t := add(iszero(x), 255)
+                r := shl(7, lt(0xffffffffffffffffffffffffffffffff, x))
+                r := or(r, shl(6, lt(0xffffffffffffffff, shr(r, x))))
+                r := or(r, shl(5, lt(0xffffffff, shr(r, x))))
+                x := shr(r, x)
+                x := or(x, shr(1, x))
+                x := or(x, shr(2, x))
+                x := or(x, shr(4, x))
+                x := or(x, shr(8, x))
+                x := or(x, shr(16, x))
+                // forgefmt: disable-next-item
+                r := sub(t, or(r, byte(shr(251, mul(x, shl(224, 0x07c4acdd))),
+                    0x0009010a0d15021d0b0e10121619031e080c141c0f111807131b17061a05041f)))
+            }
+            let map := mload(add(crystal, 0x20))
+            let mario_length := add(div(sub(255, clz(compound)), 8), 1)
+            // move to big endian
+            compound := shl(clz(compound), compound)
+            // | 16 bits    |        6 bits          | .... |
+            // | bytes used | length of last element | .... |
+            if gt(and(shr(234, map), 0x2ff), 0) {
+                // WRONG
+                return(crystal, add(crystal, mload(crystal)))
+            }
+            // error when size is full aka 32 == 32
+            // mem expansion needed if compound's size greater than the chunk left
+            // TODO : recopy entire crystal to new memory if memory collision
+            if gt(mario_length, sub(sub(mload(crystal), 0x20), shr(246, map))) {
+                log1(0, 0, 0x21)
+                mstore(crystal, add(mload(crystal), 0x20))
+            }
+            // store compound
+            mstore(add(add(crystal, 0x40), shr(246, map)), compound)
+            let new_map := or(shl(6, map), mario_length)
+            new_map := or(new_map, or(and(new_map, 0x003FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF), add(and(map, 0xFFC0000000000000000000000000000000000000000000000000000000000000), shl(246, mario_length))))
+            // update map
+            mstore(add(crystal, 0x20), new_map)
+        }
+        return crystal;
+    }
+
+    function smoke(bytes memory crystal) internal returns (uint256[40] memory smoked) {
+
+    }
+
+    function molarMass(bytes memory crystal) internal pure returns (uint256 mass) {
+        assembly {
+            mass := shr(246, mload(add(crystal, 0x20)))
+        }
+    }
+}
--- test/Greeter.t.sol
+++ test/Greeter.t.sol
@@ -2,41 +2,16 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {SevenKiloCrystal as SKC} from "src/SevenKiloCrystal.sol";
 
-import {Greeter} from "src/Greeter.sol";
+contract SevenKiloCrystalTest is Test {
 
-contract GreeterTest is Test {
-    using stdStorage for StdStorage;
-
-    Greeter greeter;
-
-    event GMEverybodyGM();
-
-    function setUp() external {
-        greeter = new Greeter("gm");
-    }
-
-    // VM Cheatcodes can be found in ./lib/forge-std/src/Vm.sol
-    // Or at https://github.com/foundry-rs/forge-std
-    function testSetGm() external {
-        // slither-disable-next-line reentrancy-events,reentrancy-benign
-        greeter.setGreeting("gm gm");
-
-        // Expect the GMEverybodyGM event to be fired
-        vm.expectEmit(true, true, true, true);
-        emit GMEverybodyGM();
-        // slither-disable-next-line unused-return
-        greeter.gm("gm gm");
-
-        // Expect the gm() call to revert
-        vm.expectRevert(abi.encodeWithSignature("BadGm()"));
-        // slither-disable-next-line unused-return
-        greeter.gm("gm");
-
-        // We can read slots directly
-        uint256 slot = stdstore.target(address(greeter)).sig(greeter.owner.selector).find();
-        assertEq(slot, 1);
-        bytes32 owner = vm.load(address(greeter), bytes32(slot));
-        assertEq(address(this), address(uint160(uint256(owner))));
+    function test() external {
+        bytes memory payload = SevenKiloCrystal.cook();
+        payload = cram(payload, uint(100));
+        payload = SKC.cram(payload, uint(2**256 - 1));
+        payload = SKC.cram(payload, uint(100));
+        console.logBytes(payload);
     }
 }
